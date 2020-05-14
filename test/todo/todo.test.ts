90a3f5cf923bfa59ca90bf17ade8522575199071
PangNem <sprnd645@gmail.com>
2020-05-15 02:13:34 +0900

    Test todo

--- test/todo/todo.test.ts
+++ test/todo/todo.test.ts
@@ -1,35 +1,35 @@
 import * as request from 'supertest';
 import app from '../../src/app';
 import sequelize from '../../src/models/sequelize';
-import User from '../../src/models/Todo';
+import Todo from '../../src/models/Todo';
 
 describe('user test', () => {
-  let testUserId: number;
+  let testTodoItemId: number;
 
-  const testData = { nickname: 'pangnem' };
+  const testData = { title: '할일1' };
 
   beforeAll(async (done) => {
     await sequelize.sync();
-    const testUser = await User.create({ testData });
+    const testTodoItem = await Todo.create({ testData });
 
-    testUserId = testUser.id;
+    testTodoItemId = testTodoItem.id;
     done();
   });
 
   afterAll(async (done) => {
-    await User.destroy({ where: { id: testUserId } });
+    await Todo.destroy({ where: { id: testTodoItemId } });
     done();
   });
 
-  describe('POST /user', () => {
-    const url = '/user';
+  describe('POST /todo', () => {
+    const url = '/todo';
 
     describe('유저 생성 성공시', () => {
       test('유저 닉네임 반환', async () => {
         const response = await request(app).post(url).send(testData);
 
         expect(response.status).toBe(201);
-        expect(response.body.data.nickname).toBe('pangnem');
+        expect(response.body.data.title).toBe('할일1');
       });
     });
     describe('유저 생성 실패시', () => {
